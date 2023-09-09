7531712448b87728c1d9279986234b3bc2e4f496
miladesmailpour <milad.esmailpour@gmail.com>
2023-09-09 00:08:08 -0400

    add the userLogin method

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 
 class UserController extends Controller
@@ -57,4 +58,49 @@ class UserController extends Controller
             ], 500);
         }
     }
+    public function userLogin(Request $request)
+{
+    try {
+        // Validate user login input data
+        $validateUser = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Check if validation fails and return errors if so
+        if ($validateUser->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'errors' => $validateUser->errors()
+            ], 401);
+        }
+
+        // Attempt to authenticate the user with provided email and password
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Email & Password do not match with our records.',
+            ], 401);
+        }
+
+        // Retrieve the authenticated user
+        $user = User::where('email', $request->email)->first();
+
+        // Return a success response with user data and an authentication token
+        return response()->json([
+            'status' => true,
+            'message' => 'User Logged In Successfully',
+            'token' => $user->createToken("API TOKEN")->plainTextToken
+        ], 200);
+
+    } catch (\Throwable $th) {
+        // Handle exceptions and return an error response
+        return response()->json([
+            'status' => false,
+            'message' => $th->getMessage()
+        ], 500);
+    }
+}
+
 }
