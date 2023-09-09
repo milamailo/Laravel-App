9996abfa7d9098d86a75a9470cbf51c2640cec27
miladesmailpour <milad.esmailpour@gmail.com>
2023-09-09 00:09:59 -0400

    login route added under api/login

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use App\Http\Controllers\Api\UserController;
 */
 
 Route::post('/register', [UserController::class, 'createUser']);
+Route::post('/login', [UserController::class, 'userLogin']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
