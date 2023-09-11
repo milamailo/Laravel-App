5ffef2f0424d678c42865217a83f7a0508af3c47
miladesmailpour <milad.esmailpour@gmail.com>
2023-09-10 23:52:45 -0400

    create the AchievementUnlocked event and LevelUpCommentAchievement

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace App\Providers;
 
+use App\Events\AchievementUnlocked;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 use App\Events\CommentWritten;
+use App\Listeners\LevelUpCommentAchievement;
 use App\Listeners\NewCommentAdd;
 
 class EventServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class EventServiceProvider extends ServiceProvider
         CommentWritten::class => [
             NewCommentAdd::class,
         ],
+        AchievementUnlocked::class => [
+            LevelUpCommentAchievement::class,
+        ]
     ];
 
     /**
